eaac2bd7a05616c7fc5b7139b49eacdf63b04107
filipkasan <filip.kasan@student.ossp.cz>
2025-04-30 12:16:49 +0200

    funguje to

---
+++ app/Presentation/Accessory/LatteExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Presentation\Accessory;
+
+use Latte\Extension;
+
+class LatteExtension extends Extension
+{
+    public function getTags(): array
+    {
+        return [];
+    }
+}
